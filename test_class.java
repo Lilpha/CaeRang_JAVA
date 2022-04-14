33ff093fa4f2891824f02c1b3fa37e9b30688abd
Kimc <32549359+Lilpha@users.noreply.github.com>
2022-04-15 01:19:20 +0900

    다중 클래스 연습

---
+++ test_class.java
@@ -0,0 +1,39 @@
+
+public class test_class {
+
+}
+class Contact{
+	private String name;
+	private String tel;
+	private String email;
+	
+	Contact(String name,String tel,String email) {
+		//전역변수 초기화
+	}
+	
+	public String getName() {
+		return this.name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getTel() {
+		return this.tel;
+	}
+	public void setTel(String tel) {
+	this.tel = tel;
+	}
+	public String getEmail() {
+		return this.email;
+		}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	
+	
+	static String toString(String str) {
+		return void;
+	}
+}
+
+
